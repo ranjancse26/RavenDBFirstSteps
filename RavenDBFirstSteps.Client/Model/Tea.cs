edab6976bbd39c3624390592ed6bf41b09ecdaf5
Kai <kai@BrainDrivenDevelopment.com>
2013-03-04 20:14:08 +0100

    Initial Comit

---
+++ RavenDBFirstSteps.Client/Model/Tea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RavenDBFirstSteps.Client.Model
+{
+    public class Tea
+    {
+        public String Id { get; set; }
+
+        public String Name { get; set; }
+
+        public TeaType TeaType { get; set; }
+
+        public Double WaterTemp { get; set; }
+
+        public Int32 SleepTime { get; set; }
+    }
+}
---
+++ RavenDBFirstSteps.Client/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Document;
+using RavenDBFirstSteps.Client.Model;
+
+namespace RavenDBFirstSteps.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tea tea = new Tea() { Name = "Earl Grey", TeaType = TeaType.Black, WaterTemp = 99, SleepTime = 3 };
+
+            StoreTea(tea);
+
+            Console.WriteLine(tea.Id + "--" + tea.Name);
+
+            tea = LoadTea("teas/1");
+
+            Console.WriteLine(tea.Id + "--" + tea.Name);
+
+            foreach (Tea dbTea in ListTeasByType(TeaType.Black))
+            {
+                Console.WriteLine(dbTea.Name);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void StoreTea(Tea tea)
+        {
+            using (var ds = new DocumentStore { Url = DATABASEURL }.Initialize())
+            {
+                using (var session = ds.OpenSession(DATABASE))
+                {
+                    session.Store(tea);
+                    session.SaveChanges();
+                    Console.WriteLine(tea.Id);
+                }
+            }
+        }
+
+        static Tea LoadTea(String id)
+        {
+            using (var ds = new DocumentStore { Url = DATABASEURL }.Initialize())
+            {
+                using (var session = ds.OpenSession(DATABASE))
+                {
+                    return session.Load<Tea>(id);
+                }
+            }
+        }
+
+        static void DeleteTea(String id)
+        {
+            using (var ds = new DocumentStore { Url = DATABASEURL }.Initialize())
+            {
+                using (var session = ds.OpenSession(DATABASE))
+                {
+                    Tea tea = session.Load<Tea>(id);
+                    session.Delete(tea);
+                    session.SaveChanges();
+                }
+            }
+        }
+
+        static List<Tea> ListTeasByType(TeaType teaType)
+        {
+            using (var ds = new DocumentStore { Url = DATABASEURL }.Initialize())
+            {
+                using (var session = ds.OpenSession(DATABASE))
+                {
+                    List<Tea> dbTeas = (from teas in session.Query<Tea>()
+                                        where teas.TeaType == teaType
+                                        select teas).ToList<Tea>();
+
+                    return dbTeas;
+                }
+            }
+        }
+
+        const String DATABASE = "RavenDBFirstSteps";
+        const String DATABASEURL = "http://localhost:8080/";
+
+    }
+}
